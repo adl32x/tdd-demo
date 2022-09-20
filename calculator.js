802ba53ba035fdba166062c9daee0e0799e77d44
Andreas Lindroos <andreas.lindroos@gmail.com>
2022-09-20 16:26:02 +0300

    Fix calculator addition and division bugs

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ function multiply (a, b) {
 }
 
 function addition (a, b) {
-  return a + b + 1;
+  return a + b;
 }
 
 function subtraction (a, b) {
@@ -11,7 +11,7 @@ function subtraction (a, b) {
 }
 
 function division (a, b) {
-  return a + b;
+  return a / b;
 }
 
 module.exports = {
